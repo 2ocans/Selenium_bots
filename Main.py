590e7f0ebd289d5af0a1171c2dc6c663d00d130d
2ocans <73264796+2ocans@users.noreply.github.com>
2021-06-11 13:37:05 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,27 @@
+import time
+import random
+from selenium import webdriver
+
+driver = webdriver.Chrome('D:\Webdrivers\chromedriver.exe')
+
+videos = [
+#Put your Videos here
+
+
+
+]
+
+sleep_time = 0
+
+for i in range(100):
+    print("botting for {} times".format(i))
+    random_video = random.randint(0,8)
+    driver.get(videos[random_video])
+    time.sleep(sleep_time) 
+    sleep_time = random.randint(10, 20)
+    time.sleep(5)
+    
+     
+driver.quit()
+
+#This is a simple template, you will need a proxy for this bot to work, otherwise it will not work.
